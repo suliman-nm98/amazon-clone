f915d64107bac9f9351e151dab073bb6309952a9
suliman <orzavon@gmail.com>
2020-09-26 17:20:28 +0400

    add header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAuhE6f_c3AQdHaRtRs_OhsmLsJJAKVTq4",
+  authDomain: "clone-62c97.firebaseapp.com",
+  databaseURL: "https://clone-62c97.firebaseio.com",
+  projectId: "clone-62c97",
+  storageBucket: "clone-62c97.appspot.com",
+  messagingSenderId: "647157622529",
+  appId: "1:647157622529:web:6d1765433cb83f31a59d00",
+  measurementId: "G-VTRPSK4C3B"
+};
