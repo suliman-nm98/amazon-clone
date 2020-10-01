03234219e2c8efd81b09ebab75b8af34766fa695
suliman <orzavon@gmail.com>
2020-10-01 19:15:16 +0400

    Deploy to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
-const express = require("express");
 const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
   "sk_test_51HWo8UKqI9vJLiaVkGFvw8qqLddTgAMydU4y8JAlOj0llCd2dlnNI4iUSCAWIZefaDHXbYUqreBG7OdXP3jN9aNN00xSe6SAuO"
